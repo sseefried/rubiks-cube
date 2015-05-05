b54df8e66d8dc9ff25b4d1dc67a1a6698faf7dfb
blonkm <blonkm@gmail.com>
2015-05-05 07:08:02 -0400

    Soften the cube colors to look more realistic
    Colors are a bit too hard, it looks unnatural.
    Closes #10

--- js/rubiks.js
+++ js/rubiks.js
@@ -390,12 +390,12 @@ function Cube(rubiksCube, coordinates, color) {
     this.translationVector = vec3.create();
     this.stickers = [];
     this.COLORS = {
-        'blue': [0.0, 0.0, 1.0, 1.0],
-        'green': [0.0, 1.0, 0.0, 1.0],
+        'blue': [0.1, 0.1, 1.0, 1.0],
+        'green': [0.1, 0.7, 0.1, 1.0],
         'orange': [1.0, 0.5, 0.0, 1.0],
-        'red': [1.0, 0.0, 0.0, 1.0],
+        'red': [0.8, 0.1, 0.1, 1.0],
         'white': [1.0, 1.0, 1.0, 1.0],
-        'yellow': [1.0, 1.0, 0.0, 1.0]
+        'yellow': [1.0, 1.0, 0.1, 1.0]
     }
     
     this.init = function() {
