7eff169bdb7c38957c53c09243175c58ec02e3ed
Sean Seefried <sean.seefried@gmail.com>
2019-05-05 20:00:00 +1000

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-BASE=/home/sseefried/sites/seanseefried.com/public/rubiks
+if [ "$SEANSEEFRIED_DOT_ORG_MACHINE" = "" ]; then
+  echo "Please set SEANSEEFRIED_DOT_ORG_MACHINE to an SSHable path"
+  exit 1
+fi
+
+if [ "$SEANSEEFRIED_DOT_ORG_PATH" = "" ]; then
+  echo "Please set SEANSEEFRIED_DOT_ORG_PATH to path of remote directory without "
+  exit 1
+fi
+
+BASE="$SEANSEEFRIED_DOT_ORG_PATH/rubiks"
+
+FULL_BASE="${SEANSEEFRIED_DOT_ORG_MACHINE}:${BASE}"
 
 for i in js/* static/main.css; do
-  ssh playspace mkdir -p $BASE/$(dirname $i)
-  scp $i playspace:$BASE/$i
+  ssh "$SEANSEEFRIED_DOT_ORG_MACHINE" mkdir -p $BASE/$(dirname $i)
+  scp $i "${FULL_BASE}/$i"
 done
 
-scp dual-solve.html playspace:$BASE/index.html
+scp dual-solve.html "${FULL_BASE}/index.html"
